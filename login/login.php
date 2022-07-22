00d0cd9eaddd4392750b3a697faf594f89dd95b8
shifana4020 <77154494+shifana4020@users.noreply.github.com>
2022-07-22 11:12:26 +0530

    Add files via upload

---
+++ login/login.php
@@ -0,0 +1,192 @@
+
+
+      <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+    <title>women empowerment</title>
+
+    <!-- Google font -->
+    <link href="https://fonts.googleapis.com/css?family=Lato:700%7CMontserrat:400,600" rel="stylesheet">
+
+    <!-- Bootstrap -->
+    <link type="text/css" rel="stylesheet" href="css/bootstrap.min.css"/>
+
+    <!-- Font Awesome Icon -->
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+
+    <!-- Custom stlylesheet -->
+    <link type="text/css" rel="stylesheet" href="css/style.css"/>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    </head>
+  <body>
+
+    <!-- Header -->
+    <header id="header" class="transparent-nav">
+      <div class="container">
+
+        <div class="navbar-header">
+          <!-- Logo -->
+          
+          <!-- /Logo -->
+
+          <!-- Mobile toggle -->
+          <button class="navbar-toggle">
+            <span></span>
+          </button>
+          <!-- /Mobile toggle -->
+        </div>
+
+        <!-- Navigation -->
+        <nav id="nav">
+          <h2 style="color: white;">WOMEN EMPOWERMENT</h2>
+          <ul class="main-menu nav navbar-nav navbar-right">
+            <li><a href="../index.php">Home</a></li>
+            <li><a href="login.php">Login</a></li>
+           <!--  <li><a href="college_registration.php">College register</a></li>
+            <li><a href="teacher_registration.php">Teacher register</a></li> -->
+          </ul>
+        </nav>
+        <!-- /Navigation -->
+
+      </div>
+    </header>
+    <!-- /Header -->
+
+    <!-- Home -->
+    <div id="home" class="hero-area">
+
+      <!-- Backgound Image -->
+      <div class="bg-image bg-parallax overlay" style="background-image:url(./img/women4.jpg)"></div>
+      <!-- /Backgound Image -->
+
+      <div class="home-wrapper">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-8">
+             <form name="sentMessage" id="contactForm"  action="../loginAcc.php" method="post" novalidate >
+        <div class="row" align="center">
+<table align="center">
+  <th><h2 style="color: cornsilk;">LOGIN</h2></th>
+          <tr><td style="color: cornsilk;">Username</td>
+            <td><div class="form-group">
+              <input type="text" id="name" class="form-control" name = "username"placeholder="username" required="required" style="
+    width: auto" >
+              <p class="help-block text-danger"></p>
+            </div></td>
+          </tr>
+          <tr><td style="color: cornsilk;">Password</td>
+            <td><div class="form-group">
+              <input type="password" id="email" class="form-control" name="password" placeholder="password" required="required" style="
+    width: auto">
+              <p class="help-block text-danger"></p>
+            </div></td>
+          </tr>
+           
+<tr><td><input type="submit" class="main-button icon-button" value="Login"></td></tr>
+<tr><td><h4>Don't have an account? <a style="color:lightblue" href="../index.php">Sign Up</a></h4></td></tr>
+</table>
+        </div>
+        <div id="success"></div>
+        
+      </form>
+             
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+    <!-- /Home -->
+
+    <!-- About -->
+    
+
+    <!-- Footer -->
+  <!--  <footer id="footer" class="section">
+
+      <!-- container -->
+      <!--<div class="container">
+
+        <!-- row -->
+      <!--  <div class="row">
+
+          <!-- footer logo -->
+         <!-- <div class="col-md-6">
+            <div class="footer-logo">
+              
+            </div>
+          </div>
+          <!-- footer logo -->
+
+          <!-- footer nav -->
+         <!-- <div class="col-md-6">
+            <ul class="footer-nav">
+              <li><a href="">Home</a></li>
+              <li><a href="index.php">About</a></li>
+              <li><a href="#">Courses</a></li>
+              <li><a href="">Blog</a></li>
+              <li><a href="">Contact</a></li>
+            </ul>
+          </div>
+          <!-- /footer nav -->
+
+      <!--  </div>
+        <!-- /row -->
+
+        <!-- row -->
+       <!-- <div id="bottom-footer" class="row">
+
+          <!-- social -->
+          <!--<div class="col-md-4 col-md-push-8">
+            <ul class="footer-social">
+              <li><a href="#" class="facebook"><i class="fa fa-facebook"></i></a></li>
+              <li><a href="#" class="twitter"><i class="fa fa-twitter"></i></a></li>
+              <li><a href="#" class="google-plus"><i class="fa fa-google-plus"></i></a></li>
+              <li><a href="#" class="instagram"><i class="fa fa-instagram"></i></a></li>
+              <li><a href="#" class="youtube"><i class="fa fa-youtube"></i></a></li>
+              <li><a href="#" class="linkedin"><i class="fa fa-linkedin"></i></a></li>
+            </ul>
+          </div>
+          <!-- /social -->
+
+          <!-- copyright -->
+         <!--<div class="col-md-8 col-md-pull-4">-->
+            <div class="footer-copyright">
+			<p>© 2022 Women Empowerment. All rights reserved </p>
+              </div>
+          
+          <!-- /copyright -->
+
+       <!-- </div>
+        <!-- row -->
+
+     <!-- </div>
+      <!-- /container -->
+
+   <!-- </footer>
+    <!-- /Footer -->
+
+    <!-- preloader -->
+ <!-- <div id='preloader'><div class='preloader'></div></div>
+    <!-- /preloader -->
+
+
+    <!-- jQuery Plugins -->
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/main.js"></script>
+
+  </body>
+</html>
